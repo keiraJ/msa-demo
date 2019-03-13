0461e120f2ce915c40eaed2ae904d8f124d32412
keira.j <keira.j@kakaocorp.com>
2019-03-13 14:56:26 +0900

    demo project Initial commit

---
+++ a-service/src/main/java/com/example/demo/services/aservice/AServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.services.aservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AServiceApplication.class, args);
+    }
+
+}
